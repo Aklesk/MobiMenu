ac4281d03ff2a7f238054c100322d8811235393f
Wolf Hatch <wolf@thaumaturgy.co.nz>
2016-07-26 23:32:31 -0700

    Re-wrote footer to be a stateless function

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 
-// This is the footer at the bottom of the page
-class Footer extends React.Component {
-    constructor() {super()}
-    render() {
-        return (
-            <footer>
-                © 2016 Mobi2Go Limited. All rights reserved.<br />
-                <a>Terms of Service</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-                <a>Privacy Policy</a>
-            </footer>
-        )
-    }
+// This is the footer at the bottom of the page. It never changes.
+export default function() {
+    return (
+        <footer>
+            © 2016 Mobi2Go Limited. All rights reserved.<br />
+            <a>Terms of Service</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a>Privacy Policy</a>
+        </footer>
+    )
 }
-
-export default Footer
