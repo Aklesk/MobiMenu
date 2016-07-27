4ad636f9f7f1b9039ea0b99e231d349b6c2ee1ee
Wolf Hatch <wolf@thaumaturgy.co.nz>
2016-07-27 01:05:39 -0700

    Converted the main body to a stateless function. All changes in it come from the router.

--- src/containers/mainBody.jsx
+++ src/containers/mainBody.jsx
@@ -6,22 +6,18 @@ import ExportOptions from '../components/exportOptions.jsx'
 // This renders main tabs at the top area
 import MainTabs from '../components/mainTabs.jsx'
 
-class MainBody extends React.Component {
-    constructor() {super()}
-    render() {
-        return (
-            <div className="main">
-                <div className="container-fluid container_12">
-                    <h1>Menu Management</h1>
-                    <div className="clear" />
-                    <div className="clear" />
-                    <ExportOptions />
-                    <MainTabs section={this.props.params.section}/>
-                    {this.props.children} 
-                </div>
+// This is a container that holds the main body. Its contents change based on the URL (due to the router)
+export default function(props) {
+    return (
+        <div className="main">
+            <div className="container-fluid container_12">
+                <h1>Menu Management</h1>
+                <div className="clear" />
+                <div className="clear" />
+                <ExportOptions />
+                <MainTabs section={props.params.section}/>
+                {props.children}
             </div>
-        );
-    }
+        </div>
+    )
 }
-
-export default MainBody
