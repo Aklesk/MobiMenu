7adae72096f8e7f85c50758c3929015a64289d45
Wolf Hatch <wolf@thaumaturgy.co.nz>
2016-07-26 23:29:18 -0700

    Re-write map function to use more compact ES6 syntax

--- src/components/exportOptions.jsx
+++ src/components/exportOptions.jsx
@@ -2,20 +2,13 @@ import React from 'react'
 import ShortID from 'shortid'
 import { exportOptions } from '../interfaceConstants.js'
 
-class ExportOptions extends React.Component {
-    constructor() {super()}
-    render() {
-        const ExportList = exportOptions.map((text) => {
-            return (
-                <a key={ShortID.generate()} style={{float: "right", textDecoration: "underline", marginRight: "20px"}}>
-                    {text}
-                </a>
-            )
-        })
-        return (
-            <div>{ExportList}</div>
-        )
-    }
+export default function() {
+    const ExportList = exportOptions.map(text =>
+        <a key={ShortID.generate()} style={{float: "right", textDecoration: "underline", marginRight: "20px"}}>
+            {text}
+        </a>
+    )
+    return (
+        <div>{ExportList}</div>
+    )
 }
-
-export default ExportOptions
