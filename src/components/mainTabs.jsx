e6caa2d51ef354ebe2f9d561842910235ca750ba
Wolf Hatch <wolf@thaumaturgy.co.nz>
2016-07-26 23:48:59 -0700

    Re-wrote to improve readability and ES6 syntax, and converted to stateless function.

--- src/components/mainTabs.jsx
+++ src/components/mainTabs.jsx
@@ -4,26 +4,19 @@ import { Link } from 'react-router';
 import { mainTabs } from '../interfaceConstants.js'
 import '../styles/mainTabs.less'
 
-class MainTabs extends React.Component {
-    constructor() {super()}
-    render() {
-        const Tabs = Object.keys(mainTabs).map((data, i) => {
-            let classText = "button"
-            data == this.props.section ? classText = "button active" : classText = "button"
-            return (
-                <Link to={`/menu/${data}`} key={ShortID.generate()} className={classText}>
-                    {mainTabs[data].label}
-                </Link>
-            );
-        });
-        return (
-            <div id="menu-navigation" className="grid_12">
-                <div className="btn-group">
-                    {Tabs}
-                </div>
+// These are the main tabs at the top of the active area that control editing section. They are dynamic,
+// but do not rely on state or pass information anywhere other than the browser URL
+export default function(props) {
+    const Tabs = Object.keys(mainTabs).map((data, i) =>
+        <Link to={`/menu/${data}`} key={ShortID.generate()} className={data == props.section ? "button active" : "button"}>
+            {mainTabs[data].label}
+        </Link>
+    )
+    return (
+        <div id="menu-navigation" className="grid_12">
+            <div className="btn-group">
+                {Tabs}
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default MainTabs
