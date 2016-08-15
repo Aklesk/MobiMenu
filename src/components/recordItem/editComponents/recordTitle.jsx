04d6f4bc7c7cb7de6ddac867a4fb9168f74362f0
Wolf Hatch <wolf@thaumaturgy.co.nz>
2016-08-15 01:42:56 -0700

    Broke out editing functionality of header.jsx into a separate file to follow a more consistent and clear pattern

---
+++ src/components/recordItem/editComponents/recordTitle.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+// These MUST BE UNIQUE
+const baseElement = "leftPart"
+const editElement = "intNameEdit"
+
+class RecordTitle extends React.Component {
+    constructor() {
+        super()
+    }
+    componentDidMount() {
+
+        // We need this as well because we want to edit this immediately after it is created in the case of
+        // a new record creation event that also drops us into editing mode.
+
+        if (document.getElementById(editElement) != null) {
+            document.getElementById(editElement).focus()
+        }
+    }
+    componentDidUpdate() {
+        if (document.getElementById(editElement) != null) {
+            document.getElementById(editElement).focus()
+        }
+    }
+    onClick = (rec, elem, event) => {
+        const { editing, record } = this.props
+        const saveFunc = () => {
+            const rec = record
+            rec.intName = document.getElementById(elem).value
+            return rec
+        }
+        editing(rec, elem, saveFunc, event)
+    }
+    render() {
+        const { editing, record } = this.props
+        return(
+            <div>
+                {
+                    editing().elem == editElement
+                        ?
+                        <div className={baseElement}>
+                            <input id="intNameEdit"
+                                   type="text"
+                                   style={{width: "326px"}}
+                                   defaultValue={record.intName}
+                                   onClick={(e) => e.stopPropagation()}
+                            />
+                        </div>
+                        :
+                        <div className={`${baseElement} editable`}
+                             onClick={this.onClick.bind(this, record.guid, "intNameEdit")}
+                        >
+                            <div className="labelText">
+                                Internal Name:
+                            </div>
+                            <div className="headerText">
+                                {
+                                    record.intName.length > 0
+                                    ?
+                                    record.intName
+                                    :
+                                    <span className="filler">-- Internal Name --</span>
+                                }
+                            </div>
+                        </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default RecordTitle
--- src/components/recordItem/header.jsx
+++ src/components/recordItem/header.jsx
@@ -2,22 +2,11 @@ import React from 'react'
 import { browserHistory } from 'react-router'
 import { mainTabs } from '../../interfaceConstants.js'
 
-// We're using componentWillUpdate and componentWillMount to get the record data out of the database to try to
-// minimize the number of database calls.
+import RecordTitle from './editComponents/recordTitle.jsx'
 
 class RecordItemHeader extends React.Component {
     constructor() {
         super()
-    }
-    componentDidMount() {
-        if (document.getElementById("intNameEdit") != null) {
-            document.getElementById("intNameEdit").focus()
-        }
-    }
-    componentDidUpdate() {
-        if (document.getElementById("intNameEdit") != null) {
-            document.getElementById("intNameEdit").focus()
-        }
     }
     deleteRecord(event) {
         const { record, section } = this.props
@@ -32,16 +21,9 @@ class RecordItemHeader extends React.Component {
             okayFunc
         )
     }
-    onClick = (rec, elem, event) => {
-        const saveFunc = () => {
-            const rec = this.props.record
-            rec.intName = document.getElementById(elem).value
-            return rec
-        }
-        this.context.editing(rec, elem, saveFunc, event)
-    }
     render() {
         const { record, section } = this.props
+        const { editing } = this.context
         return (
             <div>
                 <div className="recordHeader">
@@ -49,35 +31,7 @@ class RecordItemHeader extends React.Component {
                         <tbody>
                         <tr>
                             <td>
-                                {
-                                    this.context.editing().elem == "intNameEdit"
-                                    ?
-                                    <div className="leftPart">
-                                        <input id="intNameEdit"
-                                               type="text"
-                                               style={{width: "326px"}}
-                                               defaultValue={record.intName}
-                                               onClick={(e) => e.stopPropagation()}
-                                        />
-                                    </div>
-                                    :
-                                    <div className="leftPart editable"
-                                             onClick={this.onClick.bind(this, record.guid, "intNameEdit")}
-                                        >
-                                        <div className="labelText">
-                                            Internal Name:
-                                        </div>
-                                        <div className="headerText">
-                                            {
-                                                record.intName.length > 0
-                                                ?
-                                                record.intName
-                                                :
-                                                <span className="filler">-- Internal Name --</span>
-                                            }
-                                        </div>
-                                    </div>
-                                }
+                                <RecordTitle record={record} editing={editing} />
                             </td>
                             <td>
                                 <div className="rightPart">
