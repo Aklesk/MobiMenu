3b1e9cb0637b802ab2f3d2341c2af3ff793c993d
Wolf Hatch <wolf@thaumaturgy.co.nz>
2016-08-18 23:00:46 -0700

    More refactoring

--- src/components/menuListItem.jsx
+++ src/components/menuListItem.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+// This is a single line item of the lest of records on the left. This can be listing any of the record types
+// based on which tab is selected.
 export default class MenuListItem extends React.Component {
     static propTypes = {
         record: React.PropTypes.object.isRequired,
         section: React.PropTypes.string.isRequired
     }
     render() {
-        const { section, rec } = this.props
+        const { section, record } = this.props
         return (
-            <Link to={`/menu/${section}/${rec.guid}`}
+            <Link to={`/menu/${section}/${record.guid}`}
                   className={
-                      window.location.href.split('/')[window.location.href.split('/').length - 1] == rec.guid
+                      window.location.href.split('/')[window.location.href.split('/').length - 1] == record.guid
                       ?
                       "lineItem active"
                       :
@@ -20,9 +22,9 @@ export default class MenuListItem extends React.Component {
             >
                 <div className="lineText">
                     {
-                        rec.intName.length > 0
+                        record.intName.length > 0
                         ?
-                        rec.intName
+                        record.intName
                         :
                         <span className="filler"> -- Unnamed -- </span>
                     }
--- src/containers/mainBody.jsx
+++ src/containers/mainBody.jsx
@@ -6,8 +6,9 @@ import ExportOptions from 'components/exportOptions'
 // This renders main tabs at the top area
 import MainTabs from 'components/mainTabs'
 
-// This is a container that holds the main body. Its contents change based on the URL (due to the router in app.jsx)
+// This is a container that holds the main body.
 export default function(props) {
+    const { section } = props.params
     return (
         <div className="main">
             <div className="container-fluid container_12">
@@ -15,7 +16,7 @@ export default function(props) {
                 <div className="clear" />
                 <div className="clear" />
                 <ExportOptions />
-                <MainTabs section={props.params.section}/>
+                <MainTabs section={section}/>
                 {props.children}
             </div>
         </div>
